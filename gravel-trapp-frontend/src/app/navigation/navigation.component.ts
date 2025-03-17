e6548b5491beb5ef1fe280315c69928a956a12ea
Hanno Embregts <hanno.embregts@infosupport.com>
2025-03-17 20:57:36 +0100

    Apply formatting, add navigation

--- gravel-trapp-frontend/src/app/navigation/navigation.component.ts
+++ gravel-trapp-frontend/src/app/navigation/navigation.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {MatAnchor} from '@angular/material/button';
+import {MatToolbar} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navigation',
   imports: [
-    RouterLink
+    RouterLink,
+    MatAnchor,
+    MatToolbar
   ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
