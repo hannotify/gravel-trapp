431c69ad9f49322c38ffcd2219754ece8e6cc99b
Hanno Embregts <hanno.embregts@infosupport.com>
2025-05-09 09:22:18 +0200

    Add StandingsProjector

---
+++ src/test/java/com/github/hannotify/graveltrapp/persistence/projections/StandingsProjectorIT.java
@@ -0,0 +1,12 @@
+package com.github.hannotify.graveltrapp.persistence.projections;
+
+import com.github.hannotify.graveltrapp.persistence.repositories.RepositoryTest;
+import org.junit.jupiter.api.Test;
+
+// TODO: this should be an integration test
+class StandingsProjectorIT extends RepositoryTest {
+    @Test
+    void testCalculateStandings() {
+
+    }
+}
